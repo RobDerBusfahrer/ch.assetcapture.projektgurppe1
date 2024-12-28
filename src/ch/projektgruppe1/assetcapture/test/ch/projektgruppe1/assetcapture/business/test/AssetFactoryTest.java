61de8fa8ca408c0ee1d26cfc4ced645178c98527
robsc <robsc@DESKTOP-E7121LS>
2024-12-28 17:41:37 +0100

    AssetFactoryTest started

--- src/ch/projektgruppe1/assetcapture/test/ch/projektgruppe1/assetcapture/business/test/AssetFactoryTest.java
+++ src/ch/projektgruppe1/assetcapture/test/ch/projektgruppe1/assetcapture/business/test/AssetFactoryTest.java
@@ -1,5 +1,36 @@
 package ch.projektgruppe1.assetcapture.business.test;
 
-public class AssetFactoryTest {
+import ch.projektgruppe1.assetcapture.business.AssetFactory;
+import junit.framework.TestCase;
 
+public class AssetFactoryTest extends TestCase {
+
+	//Asset Factory to use
+	private AssetFactory theAssetfactory = null;
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Override
+	protected void setUp() throws Exception {
+
+		theAssetfactory = AssetFactory.getInstance();
+
+		super.setUp();
+	}
+
+	/**
+	 * Tests MediaFactory.
+	 */
+	public void testSaveMedia() {
+		
+		//Create Laptops
+		theClient.inputAsset("Computer", "MacMini", "23525");
+		theClient.inputAsset("Laptop", "Lenovo", "GB37");
+		theClient.inputAsset("Laptop", "Dell", "3622");
+		theClient.inputAsset("Laptop", "test", "3622");
+
+	}
 }
