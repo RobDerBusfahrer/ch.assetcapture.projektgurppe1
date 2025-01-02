154691fed2ca15bb857ee68ce5ffaae45ab2ff21
robsc <robsc@DESKTOP-E7121LS>
2025-01-02 13:44:43 +0100

    AssetDAO Test started

--- src/ch/projektgruppe1/assetcapture/test/ch/projektgruppe1/assetcapture/persistance/test/AssetDAOTest.java
+++ src/ch/projektgruppe1/assetcapture/test/ch/projektgruppe1/assetcapture/persistance/test/AssetDAOTest.java
@@ -1,5 +1,22 @@
 package ch.projektgruppe1.assetcapture.persistance.test;
 
+import ch.projektgruppe1.assetcapture.persistance.*;
+
 public class AssetDAOTest {
 
+	//The DAO Factory
+	 private DAOFactory theDAOFactory = null;
+	 
+	//The DAP
+	 private AssetDAO theAssetDAO = null;
+	 
+	 @BeforeEach
+	void setUp() throws Exception {
+		theDAOFactory = DAOFactory.getInstance();
+		theAssetDAO = theDAOFactory.createMediaDAO();
+		
+		
+		
+		
+	 }
 }
